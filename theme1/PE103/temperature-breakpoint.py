f3e2fcf716776d4458d5f9f46cf0dcbff2470632
Sajith Sasidharan <sajith@hcoop.net>
2024-04-17 10:47:16 -0500

    Add code samples

---
+++ theme1/PE103/temperature-breakpoint.py
@@ -0,0 +1,6 @@
+def celsius_to_fahrenheit(celsius):
+    breakpoint()
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+    
+print(f"0 deg C in fahrenheit: {celsius_to_fahrenheit(0)}")
---
+++ theme1/PE103/temperature-pdb.py
@@ -0,0 +1,8 @@
+import pdb; pdb.set_trace()
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+
+print(f"0 deg C in fahrenheit: {celsius_to_fahrenheit(0)}")
+
